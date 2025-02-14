a46b3ac0e8c1ed2357361efd8764c14149882b06
Smit Patel <160984002+smitRoot@users.noreply.github.com>
2025-02-14 16:03:07 -0600

    Your commit message

--- Trading_app.py
+++ Trading_app.py
@@ -20,19 +20,10 @@ st.markdown("## We provide the following services:")
 st.markdown("#### :one: stock information")
 st.write("Through this page, you can see all information about stock. ")
 
-st.markdown("#### :two: stock prediction")
-st.write("You can explore predicted closing prices for the next 30 days based on historical  stock data and advanced forecsting models. ")
+st.markdown("#### :two: stock Analysis")
+st.write("You can obtain various info such as company description, employee, website. price can be obtained through option such candlestick,line.MACD or RSI enabling greater visibility.")
 
 
 st.markdown("#### :two: stock prediction")
 st.write("You can explore predicted closing prices for the next 30 days based on historical  stock data and advanced forecsting models.use this tool to gain valuable insights into market trends and make informed investment decisions. ")
 
-
-st.markdown("#### :three: CAPM Return")
-st.write("Discover how the Capital Asset Pricing Model (CAPM) calculates the expected return of different stocks asset based on its risk and market performance. ")
-
-
-
-
-st.markdown("#### :four: CAPM Beta")
-st.write("Calculates the Beta and Expected Return for Individual Stocks . ")
